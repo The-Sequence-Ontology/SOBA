9aa48dc2d8f21493df9518d024dcb1bb05ceedf8
bmoore <bmoore@b7296065-7bf1-4d25-87cd-44ec296cc058>
2013-03-12 23:45:08 +0000

    Updates to SOBAcl from garrucha at svn switch

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+rm -f t/sobacl_test_images/sobacl_test_image_01_2L.png
+rm -f t/sobacl_test_images/sobacl_test_image_02_2L.png
+rm -f t/sobacl_test_images/sobacl_test_image_01_2L.jpeg
+rm -f t/sobacl_test_images/sobacl_test_image_03_2L.png
+rm -f t/sobacl_test_images/sobacl_test_image_04_2L.png
+rm -f t/sobacl_test_images/sobacl_test_image_03_2L.gif
+rm -f t/sobacl_test_images/sobacl_test_image_05_2L.png
+rm -f t/sobacl_test_images/sobacl_test_image_06_2L.png
+rm -f t/sobacl_test_images/sobacl_test_image_07_2L.png
